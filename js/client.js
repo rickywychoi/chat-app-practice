f21766710e2742bd4e738f66c7d0db7de6161359
rickywychoi <wchoi40@my.bcit.ca>
2019-12-19 21:12:23 -0800

    add user indicator and typing indicator

--- js/client.js
+++ js/client.js
@@ -1,13 +1,35 @@
 $(() => {
   const socket = io();
+  const adminSocket = io('/admin');
 
-  $('form').on('submit', (e) => {
+  socket.on('connect', () => {
+    $('#welcome').text(`${socket.id} has entered the chatroom.`);
+  });
+
+  socket.on('disconnect', () => {
+    $('#leave').text(`${socket.id} has left.`);
+  });
+
+  $('#m').focusin((e) => {
+    e.preventDefault();
+    $('#typing').text(`${socket.id} is typing...`);
+  });
+  
+  $('#m').focusout((e) => {
+    e.preventDefault();
+    $('#typing').text('');
+  });
+  
+
+  $('form').submit((e) => {
     e.preventDefault();
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
+    $('#typing').text('');
   });
 
   socket.on('chat message', (msg) => {
-    $('#messages').append($(`<li>${msg}</li>`));
-  })
+    $('#messages').append(`<li>${msg}</li>`);
+  });
+
 });
